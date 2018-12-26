b9992bf0a8aa382bb19accce0952ec5767dfd1a7
Simon Nyssen <simon.nyssen@outlook.com>
2018-12-26 14:58:04 +0100

    + Demande de confirmation avant la suppression d'un menu + Fonctionnement asynchrone de l'ObservableCollection des menus (reste à appliquer aux 2 autres)

--- ModifieurFermette/ViewModels/MainWindowViewModel.cs
+++ ModifieurFermette/ViewModels/MainWindowViewModel.cs
@@ -4,7 +4,9 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 using System.Windows.Input;
+using MaterialDesignThemes.Wpf;
 using ModifieurFermette.Models;
 
 // dll
@@ -21,6 +23,7 @@ namespace ModifieurFermette.ViewModels
         public ObservableCollection<ShowViewMenuDuJour> MenusAff;
         public ObservableCollection<ShowViewEvenement> EvenementsAff;
         public ObservableCollection<ShowPersonne> PersonnesAff;
+        public object MenusAffLock = new object(); // Verrou utilisé pour les opérations cross-thread sur ma collection
 
         private bool _IsAllItemsEvenementsSelected, _IsAllItemsPersonnesSelected, _IsAllItemsMenuDuJourSelected;
 
@@ -33,7 +36,7 @@ namespace ModifieurFermette.ViewModels
 
         public MainWindowViewModel()
         {
-            DeleteShowViewMenuDuJourItem = new RelayCommand(Exec => DeleteSelectedShowViewMenuDuJourItem(), CanExec => CanExecDeleteSelectedShowViewMenuDuJourItem());
+            DeleteShowViewMenuDuJourItem = new RelayCommand(Exec => ExecuteDeleteSelectedShowViewMenuDuJourItem(), CanExec => CanExecDeleteSelectedShowViewMenuDuJourItem());
         }
 
         #region Méthodes
@@ -60,6 +63,9 @@ namespace ModifieurFermette.ViewModels
             {
                 PersonnesAff.Add(new ShowPersonne(TmpPersonne));
             }
+
+            // Synchronisation de nos ObservableCollections pour opérations cross-thread
+            BindingOperations.EnableCollectionSynchronization(MenusAff, MenusAffLock);
         }
         #region Sélection DG
         /// <summary>
@@ -97,20 +103,47 @@ namespace ModifieurFermette.ViewModels
         }
         #endregion
         #region Commands
-        private void DeleteSelectedShowViewMenuDuJourItem()
+        private async void ExecuteDeleteSelectedShowViewMenuDuJourItem()
         {
-            List<ShowViewMenuDuJour> ItemsToRemove = new List<ShowViewMenuDuJour>();
-            foreach (ShowViewMenuDuJour TmpMenu in MenusAff)
-            {
-                if (TmpMenu.IsSelected)
-                    ItemsToRemove.Add(TmpMenu);
-            }
-            for (int i = 0; i < ItemsToRemove.Count; i++)
-            {
-                MenusAff.RemoveAt(MenusAff.IndexOf(ItemsToRemove[i])); // Retiré localement
-                new G_Menu(config.sChConn).Supprimer(ItemsToRemove[i].ID); // Retiré dans la DB (uniquement le menu, pas les plats
-            }
+            var ConfDialog = new Views.Dialogs.ConfirmDialog("Confirmer", "Confirmer la suppression ?");
+
+            await DialogHost.Show(ConfDialog, ConfDeleteDialogClosing);
+
         }
+
+        private async void ConfDeleteDialogClosing(object sender, DialogClosingEventArgs eventArgs)
+        {
+            if ((bool)eventArgs.Parameter == false) return;
+
+            eventArgs.Cancel();
+
+            //...now, lets update the "session" with some new content!
+            eventArgs.Session.UpdateContent(new Views.Dialogs.ProgressDialog());
+            //note, you can also grab the session when the dialog opens via the DialogOpenedEventHandler
+
+            Task RemovingItems = Task.Run(() => { // Lancement d'un thread pour la suppresion des éléments
+                lock (MenusAffLock) // Verouillage de notre ObservableCollection pour pouvoir la modifier
+                {
+                    // Suppression de l'élément
+                    List<ShowViewMenuDuJour> ItemsToRemove = new List<ShowViewMenuDuJour>();
+                    foreach (ShowViewMenuDuJour TmpMenu in MenusAff)
+                    {
+                        if (TmpMenu.IsSelected)
+                            ItemsToRemove.Add(TmpMenu);
+                    }
+                    for (int i = 0; i < ItemsToRemove.Count; i++)
+                    {
+                        MenusAff.RemoveAt(MenusAff.IndexOf(ItemsToRemove[i])); // Retiré localement
+                        new G_Menu(config.sChConn).Supprimer(ItemsToRemove[i].ID); // Retiré dans la DB (uniquement le menu, pas les plats)
+                    }
+                }
+            });
+
+            // Fermeture du Dialog
+            await RemovingItems.ContinueWith((t, _) => eventArgs.Session.Close(false), null,
+                    TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
         /// <summary>
         /// Vérifie qu'au moins un élément est sélectionné
         /// </summary>
---
+++ ModifieurFermette/Views/Dialogs/ConfirmDialog.xaml.cs
@@ -0,0 +1,44 @@
+﻿using ShowableData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ModifieurFermette.Views.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for ConfirmDialog.xaml
+    /// </summary>
+    public partial class ConfirmDialog : UserControl
+    {
+        ConfirmDialogViewModel vm;
+        public ConfirmDialog(string Header_, string Message_)
+        {
+            InitializeComponent();
+            vm = new ConfirmDialogViewModel(Header_, Message_);
+            this.DataContext = vm;
+        }
+    }
+
+    public class ConfirmDialogViewModel : ObservableData
+    {
+        private string _Header, _Message;
+        public ConfirmDialogViewModel(string Header_, string Message_)
+        {
+            Header = Header_;
+            Message = Message_;
+        }
+        public string Message { get => _Message; set { _Message = value; OnPropertyChanged("Message"); } }
+        public string Header { get => _Header; set { _Header = value; OnPropertyChanged("Header"); } }
+    }
+}
