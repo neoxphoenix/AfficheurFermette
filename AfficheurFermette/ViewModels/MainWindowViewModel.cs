af752d3a79179adeb156233c3fe37a4a1d3ac22c
Jonathan WUYTENS <Jonathan.WUYTENS@student.hel.be>
2019-01-03 00:05:58 +0100

    Progression du 2 janvier :)

---
+++ AfficheurFermette/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Input;
+using MaterialDesignThemes.Wpf;
+using Projet_AFFICHEURFERMETTE.MDF.Acces;
+using Projet_AFFICHEURFERMETTE.MDF.Classes;
+using Projet_AFFICHEURFERMETTE.MDF.Gestion;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using ShowableData; //afin d'hériter de ObservableData
+
+namespace AfficheurFermette.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// ATTRIBUTS
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
+        public ConfigClass config;
+
+        private int _OngletChoisi = 0;
+        private string _ongletActuelDesc = "Repas du jour"; //Chaine de texte (description) de l'onglet actuel
+        private string _repasEntreeDuJour, _repasPlatDuJour, _repasDessertDuJour;
+        private string _test;
+        private string _dateAjd; //stocke la date du jour
+        private string _aSonAnnifAjd = "";
+
+        private ICommand _ChangeTabByClick;
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// CONSTRUCTEUR
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
+        public MainWindowViewModel()
+        {
+            ChangeTabByClick = new RelayCommand(ICmd_ExecChangeTabByClick, ICmd_CanChangeTabByClick);
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
+        /// ACCESSEURS
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
+        //Get-Set choix onglets
+        public int OngletChoisi
+        {
+            get { return _OngletChoisi; }
+            set
+            {
+                if (_OngletChoisi != value)
+                {
+                    _OngletChoisi = value;
+                    switch(_OngletChoisi)
+                    {
+                        case 0: ongletActuelDesc = "Repas du jour"; break;
+                        case 1: ongletActuelDesc = "Actualités"; break;
+                        case 2: ongletActuelDesc = "Ateliers"; break;
+                        case 3: ongletActuelDesc = "Educateurs présent aujourd'hui"; break;
+                        default: ongletActuelDesc = "Repas du jour"; break;
+                    }
+
+                    OnPropertyChanged(); //Signal le changement
+                }
+            }
+        }
+
+        //Get-Set choix onglets
+        public string ongletActuelDesc
+        {
+            get { return _ongletActuelDesc; }
+            set
+            {
+                if (_ongletActuelDesc != value)
+                    _ongletActuelDesc = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #region RelayCmd + ACCESSEURS
+
+         //RelayCmd lors du clic sur un onglet
+        public ICommand ChangeTabByClick
+        {
+            get { return _ChangeTabByClick; }
+            set
+            {
+                if (_ChangeTabByClick != value)
+                    _ChangeTabByClick = value;
+            }
+        }
+
+        public string test
+        {
+            get
+            {
+                return _test;
+            }
+            set
+            {
+                _test = value;
+            }
+        }
+
+
+        public string repasEntreeDuJour
+        {
+            get { return _repasEntreeDuJour; }
+            set
+            {
+                if (_repasEntreeDuJour != value)
+                    _repasEntreeDuJour = value;
+                OnPropertyChanged();
+            }
+        }
+        public string repasPlatDuJour
+        {
+            get { return _repasPlatDuJour; }
+            set
+            {
+                if (_repasPlatDuJour != value)
+                    _repasPlatDuJour = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string repasDessertDuJour
+        {
+            get { return _repasDessertDuJour; }
+            set
+            {
+                if (_repasDessertDuJour != value)
+                    _repasDessertDuJour = value;
+                OnPropertyChanged();
+            }
+        }
+        public string dateAjd
+        {
+            get { return DateTime.Now.ToString("dd/MM/yy"); }
+            set
+            {
+                _dateAjd = DateTime.Now.ToString("dd/MM/yy");
+                OnPropertyChanged();
+            }
+        }
+        
+        //retourne un string contenant les personnes ayant leur anniversaire ajd
+        public string aSonAnnifAjd
+        {
+           get { return _aSonAnnifAjd; }
+           set
+           {
+                if (_aSonAnnifAjd != value)
+                    _aSonAnnifAjd = value;
+                OnPropertyChanged();
+           }
+    }
+
+        //CanChange
+        private bool ICmd_CanChangeTabByClick(object o)
+        {
+            return true;
+        }
+
+        //Exec
+        private void ICmd_ExecChangeTabByClick(object param)
+        {
+            OngletChoisi = Convert.ToInt32(param); //récupère l'argument et le converti en int
+        }
+        #endregion
+
+
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
+        //FONCTIONS
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        //EVENT HANDLER qui signal un changement d'état
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        // CHARGER DONNNES DU JOUR
+        public void ChargerDonneesDaily()
+        {
+            GetMenuDuJour();
+            CheckAnnifToday();
+        }
+
+        //Menu du jour
+        public void GetMenuDuJour()
+        {
+            List<C_ViewMenuDuJour> ListeDesMenus = new G_ViewMenuDuJour(config.sChConn).Lire("");
+            C_ViewMenuDuJour MenuDuJour = ListeDesMenus.Find(x => x.Date.ToString(("dd/MM/yy")) == dateAjd);
+            if (MenuDuJour != null)
+            {
+                repasEntreeDuJour = MenuDuJour.eNom.ToString();
+                repasPlatDuJour = MenuDuJour.pNom.ToString();
+                repasDessertDuJour = MenuDuJour.dNom.ToString();
+            }
+        }
+        //Check Anniversaire du jour
+        public void CheckAnnifToday()
+        {
+            string DateAjd = DateTime.Now.ToString("dd/MM");
+            string TmpaSonAnnifAjd = "";
+            string separateur = "";
+            int i = 0;
+            List<C_Personne> AnniversaireDuJour2 = new G_Personne(config.sChConn).Lire("");
+            foreach (C_Personne aSonAnnif in AnniversaireDuJour2)
+            {
+                string _annif = aSonAnnif.DateNaissance.ToString().Substring(0, 5);
+                if (i > 0)
+                    separateur = " et "; //rajoute un séparateur si + d'une personne on leur annif
+                if (string.Compare(_annif, DateAjd) == 0)
+                {
+                    TmpaSonAnnifAjd += separateur + aSonAnnif.Nom + " " + aSonAnnif.Prenom; //détecte si quelqu'un à son annif
+                    i++;
+                }
+                    
+            }
+            aSonAnnifAjd = TmpaSonAnnifAjd;
+        }
+
+        public void GenerationDesEvenementsExistant()
+        {
+            List<C_ViewEvenement> Evenements = new G_ViewEvenement(config.sChConn).Lire("");
+
+        }
+    }
+
+
+}
---
+++ AfficheurFermette/Views/MainWindow.xaml.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using AfficheurFermette.ViewModels; //Utiliser les ViewModels
+using System.IO; 
+using Projet_AFFICHEURFERMETTE.MDF.Classes;
+using Projet_AFFICHEURFERMETTE.MDF.Acces;
+using Projet_AFFICHEURFERMETTE.MDF.Gestion;
+using ShowableData; 
+
+
+namespace AfficheurFermette
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class MainWindow : Window
+    {
+        //Attributs
+		private string sChConn = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + System.AppDomain.CurrentDomain.BaseDirectory + "AfficheurFermette.mdf;Integrated Security=True;Connect Timeout=30";
+        private MainWindowViewModel vm;
+
+        //Constructeur
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            vm = new MainWindowViewModel();
+            this.DataContext = vm; //tell the View about what its ViewModel via DataContext
+
+            // On vérifie si le fichier de config existe
+            if (File.Exists(System.AppDomain.CurrentDomain.BaseDirectory + "config.xml"))
+                vm.config = ConfigClass.DeserializeFromFile(System.AppDomain.CurrentDomain.BaseDirectory + "config.xml"); // Si oui, on le charge
+            else
+                vm.config = new ConfigClass(); // sinon on créée une nouvelle config
+
+            // On récupère uniquement le chemin d'accès du fichier à partir du ConnectionString
+            string[] stab = vm.config.sChConn.Split('=');
+            string[] stab2 = stab[2].Split(';');
+            OpenFileDialog dlgChargerDB = new OpenFileDialog();
+
+            // Si le fichier n'existe pas on propose à l'utilisateur d'indiquer l'emplacement de la base de données
+            if (!File.Exists(stab2[0]))
+            {
+                bool boucle = false;
+                do
+                {
+                    if (MessageBox.Show("La base de donnée par défaut est introuvable.\nSouhaitez-vous indiquer un autre emplacement ?", "Base de données introuvable", MessageBoxButton.YesNo, MessageBoxImage.Exclamation) == MessageBoxResult.Yes)
+                    {
+                        dlgChargerDB.Filter = "Fichier de base de données Microsoft SQL|*.mdf|Tous fichiers|*.*";
+                        if (dlgChargerDB.ShowDialog() == true)
+                        {
+                            if (File.Exists(dlgChargerDB.FileName) && System.IO.Path.GetExtension(dlgChargerDB.FileName) == ".mdf")
+                            {
+                                vm.config.sChConn = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + dlgChargerDB.FileName + ";Integrated Security=True";
+                                boucle = false;
+                                // On sauvegarde la config
+                                vm.config.SerializeToFile(System.AppDomain.CurrentDomain.BaseDirectory + "config.xml");
+                            }
+                            else
+                            {
+                                MessageBox.Show("Ce fichier n'est pas valide !");
+                                boucle = true;
+                            }
+                        }
+                        else
+                            boucle = true;
+                    }
+                    else // L'utilisateur refuse d'indiquer l'emplacement de la DB => On ferme le programme
+                    {
+                        boucle = false;
+                        this.Close();
+                    }
+                }
+                while (boucle);
+            }
+
+            // Le fichier existe
+            if (File.Exists(stab2[0]) || File.Exists(dlgChargerDB.FileName))
+            {
+                vm.ChargerDonneesDaily();
+                // liaison des données avec les DataGrid
+                //DGevenements.ItemsSource = vm.EvenementsAff;
+                //dgpersonnes.ItemsSource = vm.PersonnesAff;
+                //DGmenus.ItemsSource = vm.MenusAff;
+
+            }
+
+            //if (System.IO.File.Exists(stab2[0]) || System.IO.File.Exists(dlgChargerDB.FileName))
+            //{
+            //             // tests
+            //             List<C_ViewMenuDuJour> menus = new G_ViewMenuDuJour(sChConn).Lire("");
+            //             C_ViewMenuDuJour menu = new G_ViewMenuDuJour(sChConn).Lire_ID(1);
+            //             menu = new G_ViewMenuDuJour(sChConn).Lire_Date(new DateTime(2018, 12, 12));
+
+            //             List<C_ViewEvenement> evenements = new G_ViewEvenement(sChConn).Lire("");
+            //             C_ViewEvenement evenement = new G_ViewEvenement(sChConn).Lire_ID(1);
+            //             evenements = new G_ViewEvenement(sChConn).Lire_DateDebut(new DateTime(2018, 12, 12));
+            //             evenements = new G_ViewEvenement(sChConn).Lire_DateFin(new DateTime(2018, 12, 13));
+            //             evenements = new G_ViewEvenement(sChConn).Lire_Date(new DateTime(2018, 12, 12));
+
+            //}
+        }
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			this.WindowStyle = WindowStyle.None;
+			this.WindowState = WindowState.Maximized;
+		}
+
+		private void Window_KeyDown(object sender, KeyEventArgs e)
+		{
+			//if (e.Key == Key.Return)
+			//{
+			//	// On veut modifier les données => Lancement d'un nouvelle fenêtre
+			//	MessageBox.Show("Test");
+			//}
+			if (e.Key == Key.Escape)
+			{
+				// On veut quitter le programme
+				if (MessageBox.Show("Êtes-vous sûr de vouloir fermer l'application ?", "Confirmer", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+					this.Close();
+			}
+		}
+
+        #region Effets sur les onglets (désactivé pour le moment)
+        private void EffetOnglet(Canvas ChoixCnv)
+        {
+            CnvOnglet1.Children.Clear();
+            CnvOnglet2.Children.Clear();
+            CnvOnglet3.Children.Clear();
+            CnvOnglet4.Children.Clear();
+
+            Rectangle EffetBordure = new Rectangle();
+            EffetBordure.Fill = Brushes.DarkBlue;
+            EffetBordure.Width = ImgOnglet2.ActualWidth; //les onglets ont tous la même taille, donc OK
+            EffetBordure.Height = 2;
+            EffetBordure.StrokeThickness = 2;
+
+            ChoixCnv.Children.Add(EffetBordure);
+            Canvas.SetLeft(EffetBordure, 0);
+            Canvas.SetTop(EffetBordure, 0);
+        }
+
+        private void ImgOnglet1_Click(object sender, RoutedEventArgs e)
+        {
+            EffetOnglet(CnvOnglet1);
+        }
+
+        private void ImgOnglet2_Click(object sender, RoutedEventArgs e)
+        {
+            EffetOnglet(CnvOnglet2);
+        }
+
+        private void ImgOnglet3_Click(object sender, RoutedEventArgs e)
+        {
+            EffetOnglet(CnvOnglet3);
+        }
+
+        private void ImgOnglet4_Click(object sender, RoutedEventArgs e)
+        {
+            EffetOnglet(CnvOnglet4);
+        }
+        #endregion
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string DateAjd = DateTime.Now.ToString("dd/MM");
+            string aSonAnnifAjd = "";
+            string separateur = "";
+            int i = 0;
+            List<C_Personne> AnniversaireDuJour2 = new G_Personne(vm.config.sChConn).Lire("");
+            foreach (C_Personne aSonAnnif in AnniversaireDuJour2)
+            {
+                string _annif = aSonAnnif.DateNaissance.ToString().Substring(0, 5);
+                if (i > 0) separateur = " et "; //rajoute un séparateur si + d'une personne on leur annif
+                if (string.Compare(_annif, DateAjd) == 0) aSonAnnifAjd += separateur + aSonAnnif.Nom + " " + aSonAnnif.Prenom; //détecte si quelqu'un à son annif
+                i++;
+                tbTest.Text = aSonAnnifAjd;
+            }
+            
+        }
+    }
+}
